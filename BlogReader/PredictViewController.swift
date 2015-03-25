9fc5e7a24ffaf2b2f07b012e8b2c1ad9bb7ca5df
naoyashiga <naoyashiga0@gmail.com>
2015-03-25 22:25:09 +0900

    PageMenuライブラリの導入

---
+++ BlogReader/PredictViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PredictViewController.swift
+//  BlogReader
+//
+//  Created by naoyashiga on 2015/03/25.
+//  Copyright (c) 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class PredictViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
